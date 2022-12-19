0577981a793087ff2e49a6f1388e86b349938b79
andersonvieirarj <andersonvieirarj@gmail.com>
2022-12-19 15:46:07 -0300

    Programa finalizado

--- src/Boletim.java
+++ src/Boletim.java
@@ -2,7 +2,6 @@
 4 notas e a média. */
 
 public class Boletim {
-
     private String nome;
     private String disciplina;
     private double n1;
@@ -10,6 +9,53 @@ public class Boletim {
     private double n3;
     private double n4;
     private double media;
+    // Métodos acessores para utilização dos atributos
+    // encapsulados:
+    // SET - método que permite ALTERAR os valores dos atributos
+    // GET - metodo que permite ACESSAR, VISUALIZAR os valores
+       // dos atributos
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDisciplina() {
+        return disciplina;
+    }
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+    public double getN1() {
+        return n1;
+    }
+    public void setN1(double n1) {
+        this.n1 = n1;
+    }
+    public double getN2() {
+        return n2;
+    }
+    public void setN2(double n2) {
+        this.n2 = n2;
+    }
+    public double getN3() {
+        return n3;
+    }
+    public void setN3(double n3) {
+        this.n3 = n3;
+    }
+    public double getN4() {
+        return n4;
+    }
+    public void setN4(double n4) {
+        this.n4 = n4;
+    }
+    public double getMedia() {
+        return media;
+    }
+    public void setMedia(double media) {
+        this.media = media;
+    }
 
     // método para calcular a média do aluno
     // esse método não tem retorno, pois é do tipo void
@@ -21,6 +67,11 @@ public class Boletim {
     public double calcularMedia2(){
         media = (n1+n2+n3+n4)/4;
         return media;
+    }
+
+    public void calcularMedia3 (double n1){
+        double x = n1 * 2;
+        System.out.println(x);
     }
 
 }
--- src/Principal.java
+++ src/Principal.java
@@ -5,16 +5,33 @@ public class Principal {
         Boletim Edu = new Boletim();
         Scanner input = new Scanner (System.in);
 
-        Edu.nome = "Anderson Vieira";
+        /*Edu.nome = "Anderson Vieira";
         Edu.disciplina = "Matemática";
         Edu.n1 = 9.5;
         Edu.n2 = 5.5;
         Edu.n3 = 1.5;
-        Edu.n4 = 6.0;
+        Edu.n4 = 6.0; */
 
+        // Entrada de dados via SET
+        Edu.setNome("Anderson Vieira");
+
+
+        Edu.setDisciplina("Matemática");
+        Edu.setN1(9.5);
+        Edu.setN2(5.5);
+        Edu.setN3(1.5);
+        Edu.setN4(6.0);
+        // Entrada de dados via SET, pelo Scanner, ou seja, usuário
+        System.out.println("Digite o nome do aluno: ");
+        Edu.setNome(input.nextLine());
+        // Saída de dados vai GET, usando o SOUT
+        System.out.println(Edu.getMedia());
         Edu.calcularMedia();
         System.out.println("A média do aluno é: " + Edu.calcularMedia2());
 
+        Edu.calcularMedia3(5.5);
+        double n1 = 6.5;
+        Edu.calcularMedia3(n1);
 
     }
 }
